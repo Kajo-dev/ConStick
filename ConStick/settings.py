8cdad7fd576d18f0dc64a9c6a1b18a66fa166ba4
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-04-10 18:03:49 +0200

    schoolplans

--- ConStick/settings.py
+++ ConStick/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #moje
     'dayplan',
+    'schoolplan',
 ]
 
 MIDDLEWARE = [
--- schoolplan/models.py
+++ schoolplan/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class school(models.Model):
+    Poniedzialek=models.CharField(
+        max_length=11,
+        default='07:00-15:40'
+    )
+    Wtorek=models.CharField(
+        max_length=11,
+        default='07:00-15:40'
+    )
+    Sroda=models.CharField(
+        max_length=11,
+        default='07:00-15:40'
+    )
+    Czwartek=models.CharField(
+        max_length=11,
+        default='07:00-15:40'
+    )
+    Piatek=models.CharField(
+        max_length=11,
+        default='07:00-15:40'
+    )
+    Sobota=models.CharField(
+        max_length=11,
+        default='07:00-15:40'
+    )
+
