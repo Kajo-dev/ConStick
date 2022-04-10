2ffdc5d0b45be53f3f347ef335174208b5964f04
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-04-10 09:50:44 +0200

    dayplans

--- dayplan/models.py
+++ dayplan/models.py
@@ -1,15 +1,35 @@
 from django.db import models
 
 class plans(models.Model):
+
+    zadanie=models.CharField(
+        default="",
+        max_length=20,
+    )
+
     data=models.DateField()
+
+    #Grupa
     OSOBISTE='OS'
     PRACA='PR'
-    grupy=[(OSOBISTE,'osobiste'),(PRACA,'praca')]
-
+    SZKOLA='SZ'
+    grupy=[(OSOBISTE,'Osobiste'),(PRACA,'Praca'),(SZKOLA,'Szkola')]
     grupa = models.CharField(
         max_length=2,
         choices=grupy,
         default=OSOBISTE,
     )
     
-    
+    czas=models.CharField(
+        max_length=5,
+        default='01:00',
+    )
+
+    #zrobione
+    TAK='tak'
+    NIE='nie'
+    zrobione=[(TAK,'Tak'),(NIE,'Nie')]
+    zrobione = models.CharField(
+        max_length=3,
+        choices=zrobione,
+    )
